442b86968ff3f6f3ee8a3ffb2f61953794833054
Hantaehwan <gksxoghkskr@naver.com>
2024-07-08 10:24:28 +0900

    key manager

--- src/main/java/store/novabook/batch/common/config/DataSourceConfig.java
+++ src/main/java/store/novabook/batch/common/config/DataSourceConfig.java
@@ -1,28 +1,14 @@
 package store.novabook.batch.common.config;
 
-import java.util.Map;
-import java.util.Objects;
-
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.autoconfigure.batch.BatchDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.RequiredArgsConstructor;
-import store.novabook.batch.common.dto.DatabaseConfigDto;
 import store.novabook.batch.common.util.KeyManagerUtil;
 
 @Configuration
@@ -51,9 +37,4 @@ public class DataSourceConfig {
 		String keyid = environment.getProperty("nhn.cloud.keyManager.batchKeyId");
 		return KeyManagerUtil.getDataSource(environment, keyid);
 	}
-
-
-
-
-
 }
