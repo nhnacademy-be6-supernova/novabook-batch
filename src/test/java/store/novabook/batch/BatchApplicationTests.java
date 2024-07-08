3ace29fca46afb5dd89b384e0ea1f0bb4f8b77b4
Hantaehwan <gksxoghkskr@naver.com>
2024-07-08 10:46:42 +0900

    key manager

---
+++ src/test/java/store/novabook/batch/BatchApplicationTests.java
@@ -0,0 +1,15 @@
+package store.novabook.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
